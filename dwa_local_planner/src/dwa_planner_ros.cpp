3b3ec64fb73454408ab51b495e251bcac16aa11f
helenzhangyc <zychelenzhang@gmail.com>
2025-11-13 15:42:39 +0800

    commented out some ros debug msgs

--- dwa_local_planner/src/dwa_planner_ros.cpp
+++ dwa_local_planner/src/dwa_planner_ros.cpp
@@ -280,7 +280,7 @@ namespace dwa_local_planner {
       ROS_WARN_NAMED("dwa_local_planner", "Received an empty transformed plan.");
       return false;
     }
-    ROS_DEBUG_NAMED("dwa_local_planner", "Received a transformed plan with %zu points.", transformed_plan.size());
+    // ROS_DEBUG_NAMED("dwa_local_planner", "Received a transformed plan with %zu points.", transformed_plan.size());
 
     // update plan in dwa_planner even if we just stop and rotate, to allow checkTrajectory
     dp_->updatePlanAndLocalCosts(current_pose_, transformed_plan, costmap_ros_->getRobotFootprint());
